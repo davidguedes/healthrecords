1728a0456796494a62d9066e3dd12e8e1c4917c2
David Guedes <d2vid.guedes@gmail.com>
2022-10-05 13:09:41 -0300

    Configuração de variáveis de ambiente, além da implementação do chat, que de fato está se comunicando com o DialogFlow e realizando a conversação.

--- App.js
+++ App.js
@@ -1,6 +1,102 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+import {GiftedChat} from 'react-native-gifted-chat';
+import {Dialogflow_V2} from 'react-native-dialogflow';
 
-export default () => {
-  return <Text>Olá, mundo!</Text>;
+import {dialogflowConfig} from './env';
+
+const botAvatar = require('./assets/images/robo.png');
+
+const BOT = {
+  _id: 2,
+  name: 'Mr. Bot',
+  avatar: botAvatar,
 };
+
+class App extends Component {
+  state = {
+    messages: [
+      {
+        _id: 2,
+        text: 'Meu nome é Mr. Health 🤖',
+        createdAt: new Date(),
+        user: BOT,
+      },
+      {_id: 1, text: 'Olá!', createdAt: new Date(), user: BOT},
+    ],
+    id: 1,
+    name: '',
+  };
+
+  componentDidMount() {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_PORTUGUESE_BRAZIL,
+      dialogflowConfig.project_id,
+    );
+  }
+
+  handleGoogleResponse(result) {
+    console.log('result: ', result);
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+
+    this.sendBotResponse(text);
+  }
+
+  sendBotResponse(text) {
+    let msg = {
+      _id: this.state.messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT,
+    };
+
+    this.setState(previouseState => ({
+      messages: GiftedChat.append(previouseState.messages, [msg]),
+    }));
+  }
+
+  onSend(messages = []) {
+    this.setState(previouseState => ({
+      messages: GiftedChat.append(previouseState.messages, messages),
+    }));
+
+    let message = messages[0].text;
+
+    Dialogflow_V2.requestQuery(
+      message,
+      result => this.handleGoogleResponse(result),
+      error => console.log(error),
+    );
+  }
+
+  onQuickReply(quickReply) {
+    this.setState(previouseState => ({
+      messages: GiftedChat.append(previouseState.messages, quickReply),
+    }));
+
+    let message = quickReply[0].value;
+
+    Dialogflow_V2.requestQuery(
+      message,
+      result => this.handleGoogleResponse(result),
+      error => console.log(error),
+    );
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, backgroundColor: '#FFFFFF'}}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={message => this.onSend(message)}
+          onQuickReply={quickReply => this.onQuickReply(quickReply)}
+          user={{_id: 1}}
+        />
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ env.js
@@ -0,0 +1,14 @@
+export const dialogflowConfig = {
+  type: 'service_account',
+  project_id: 'healthrecords-qjdq',
+  private_key_id: '667b13d0fc8ff4c6800b361f9a45cceacb266c7b',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDYS9y/lKtfIpda\n3Hkz/cFv39nf9cAn1iCvBAlqfLQu8HWhJsntFQmWBMW+7J/Dmm6SFcN8rB2nmyh6\nyM40m+zBdDEtFOY3qjQL012XquQB/nxmXpLWYuQMgFAtonFE8rfnTlUWquLMeNBa\nCIKE6Yp2uJ59pwj2Glzy8YE0Kg6yD77/ErD+ibWvRGeOApaWUw0wu1sPmsZnqfE4\nXpSkkPmL9T1NUdmBGsFbKHvnOjRfcxToHDMLgGom0fpXalzxGWMWX0kvLvHcgffO\nJ1PigTlKTsXEJ+ygHgOCqh7v+nGpHfE6tQH/bTdPyPEN8GZH0WziB+YLwae0lsC3\n7JLPAg3fAgMBAAECggEAAw4cOgHEGGRtnqkaxdo55NukIjkuyLdVHa2q8/dgrw6z\n4usk0s8dbysiBQOz7DT+mCL0srahlZClyxIN3V94ys2g9xZb93sKR9jk9nbyMiUn\nEdjAMrFLGeCV9k1lZVr83CQN1MvkgI0ATybnaZoXbYVh2aXRAHvvU68DLXYIjGpl\nGGjDOyJbboDt08Ed8pSh7qDL5FOan8H+3ywkUnBhHY3N2e6eG0s/cJ2F5MGnBX1e\ng9LQ2lOhZLVRT0kMnGLSdVA1Isz/2F31FmUUrwkmLrDcU0X0HMj4A8AO6XM1iIU/\nJ3STFmLH5Q52COiGwCWI4S7X6PXdklX4iyorotbcWQKBgQD+c3Wth/825HdDgO9E\n/m3RJZUVlhuDNf1ZXYEhPv7nV9jPelZESwAVtGe2iA7kWc4RjMtPnLOLrU6D4OnV\ndE8BtXv+6NNwzGGLRYnozw4QMMf1YnGN/GDfWP0Kz+QKNSuLu2jjI7YooG/M6y2n\n93GO4Ly0AVHPKUStru0KGRyFvQKBgQDZnPEZO/Mrp92iAd3ktN5BWZL+PjxBamVd\nWfXIsyUBKWU//86NN0FiiW4JeA0ASdqkwHEnNJwtnhQ8nIw2EhzuTjaFpSOCjeDV\nrZQW1E8cSgQWQTZL4Lqo9zgE/mt2W0VO/3QUbVJuBoQMSdZqZlflrvoymbbKkz9t\nHW/lshGVywKBgFqWeHrDRBUi2WLq8EQ74JTpMF5zBnB8NCowo9xWNSHfoIydHUFL\nAki35YSSg9rALiEJYwiA3VsI5D8HP7iCo+4T0oU6Fqy91qxioWqE7JjPBSFTXuIR\ny9s4Lz4+LQ2FSG+C1SCogh2MoMDvqV13NrKLSCM9pQZ/xree4z0JG6JxAoGAdyQ2\n2DL7YtZoQK+29eWUncDV+r7GLKZ80sxTBcwXIXixh4WdZ7Ft6e4GQcbpj1PgEj2O\ndgGW1F7tA3ZOSbhHAK3vqeIM/AiDpVmAb7NTkNBxRmHzwT1L1QBtOka1PTqhSqwP\nuLNV4wlMOJIgg2Z/DzBqqVOBQY22tx3byq1gdVsCgYBT4LHpjmSjV4SHMpWto7Y/\ng2cGNlqaHu5WELmesdU14HeOS/sCKr6oe8w2bsKIj0/WKsagIraCKCE/RjbHuSwU\nKFcSb0lTawYfqHMoRehzTcf8a07PgH/3q52awlI2Mb8BmuYk59AO8WYayutI+FvM\nsgTlQsVAGeToH7mqCrt5Pw==\n-----END PRIVATE KEY-----\n',
+  client_email: 'healthrecords@healthrecords-qjdq.iam.gserviceaccount.com',
+  client_id: '101143201503849032652',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/healthrecords%40healthrecords-qjdq.iam.gserviceaccount.com',
+};
