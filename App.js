494fc77c7df466184800c8ac9c52bc9b5df5c49c
David Guedes <d2vid.guedes@gmail.com>
2022-11-28 00:59:40 -0300

    Ajustes em ícones e nas exibições do chat.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
-import {GiftedChat} from 'react-native-gifted-chat';
+import {View} from 'react-native';
+import {GiftedChat, Bubble} from 'react-native-gifted-chat';
 import {Dialogflow_V2} from 'react-native-dialogflow';
 
 import {dialogflowConfig} from './env';
@@ -9,7 +9,7 @@ const botAvatar = require('./assets/images/robo.png');
 
 const BOT = {
   _id: 2,
-  name: 'Mr. Bot',
+  name: 'Mr. BotHealth',
   avatar: botAvatar,
 };
 
@@ -45,12 +45,151 @@ class App extends Component {
   }
 
   sendBotResponse(text) {
-    let msg = {
-      _id: this.state.messages.length + 1,
-      text,
-      createdAt: new Date(),
-      user: BOT,
-    };
+    let binaryOpt = 'binary-opt';
+    let qualityOpt = 'quality-opt';
+    let specialtyOpt = 'specialty-opt';
+    let msg;
+
+    /*if (text == 'tavel') {
+      msg = {
+        _id: this.state.messages.length + 1,
+        text,
+        image: 'https://www.state.gov/wp-content/uploads/2020/11/shutterstock_186964970-scaled.jpg',
+        createdAt: new Date(),
+        user: BOT,
+      };
+    } else */
+    if (text.indexOf(binaryOpt) != -1) {
+      text = text.replace(binaryOpt, '');
+
+      msg = {
+        _id: this.state.messages.length + 1,
+        text,
+        createdAt: new Date(),
+        quickReplies: {
+          type: 'radio',
+          keepIt: true,
+          values: [
+            {
+              title: 'Sim',
+              value: 'Sim',
+              bColor: '#005C23',
+              bgColor: '#005C23',
+            },
+            {
+              title: 'Não',
+              value: 'Não',
+              bColor: '#5C0000',
+              bgColor: '#5C0000',
+            },
+          ],
+        },
+        user: BOT,
+      };
+
+      console.log('Valor de mensagem: ', msg);
+    } else if (text.indexOf(specialtyOpt) != -1) {
+      text = text.replace(specialtyOpt, '');
+
+      msg = {
+        _id: this.state.messages.length + 1,
+        text,
+        createdAt: new Date(),
+        quickReplies: {
+          type: 'radio',
+          keepIt: true,
+          values: [
+            {
+              title: 'Ortopedista',
+              value: 'Ortopedista',
+              bColor: '#FF5DE4',
+              bgColor: '#FF5DE4',
+            },
+            {
+              title: 'Cardiologista',
+              value: 'Cardiologista',
+              bColor: '#FFE25C',
+              bgColor: '#FFE25C',
+            },
+            {
+              title: 'Dermatologista',
+              value: 'Dermatologista',
+              bColor: '#009665',
+              bgColor: '#009665',
+            },
+            {
+              title: 'Pediatra',
+              value: 'Pediatra',
+              bColor: '#00B3FF',
+              bgColor: '#00B3FF',
+            },
+            {
+              title: 'Ginecologista',
+              value: 'Ginecologista',
+              bColor: '#FF0097',
+              bgColor: '#FF0097',
+            },
+            {
+              title: 'Urologista',
+              value: 'Urologista',
+              bColor: '#547735',
+              bgColor: '#547735',
+            },
+            {
+              title: 'Clínico Geral',
+              value: 'Clínico Geral',
+              bColor: '#005E7A',
+              bgColor: '#005E7A',
+            },
+          ],
+        },
+        user: BOT,
+      };
+
+      console.log('Valor de mensagem: ', msg);
+    } else if (text.indexOf(qualityOpt) != -1) {
+      text = text.replace(qualityOpt, '');
+
+      msg = {
+        _id: this.state.messages.length + 1,
+        text,
+        createdAt: new Date(),
+        quickReplies: {
+          type: 'radio',
+          keepIt: true,
+          values: [
+            {
+              title: 'Bom',
+              value: 'Bom',
+              bColor: '#005C23',
+              bgColor: '#005C23',
+            },
+            {
+              title: 'Regular',
+              value: 'Regular',
+              bColor: '#B3A700',
+              bgColor: '#B3A700',
+            },
+            {
+              title: 'Ruim',
+              value: 'Ruim',
+              bColor: '#5C0000',
+              bgColor: '#5C0000',
+            },
+          ],
+        },
+        user: BOT,
+      };
+
+      console.log('Valor de mensagem: ', msg);
+    } else {
+      msg = {
+        _id: this.state.messages.length + 1,
+        text,
+        createdAt: new Date(),
+        user: BOT,
+      };
+    }
 
     this.setState(previouseState => ({
       messages: GiftedChat.append(previouseState.messages, [msg]),
@@ -72,11 +211,21 @@ class App extends Component {
   }
 
   onQuickReply(quickReply) {
-    this.setState(previouseState => ({
-      messages: GiftedChat.append(previouseState.messages, quickReply),
-    }));
-
     let message = quickReply[0].value;
+
+    let msg = {
+      _id: this.state.messages.length + 1,
+      text: message,
+      createdAt: new Date(),
+      user: {
+        _id: 1,
+      },
+    };
+
+    console.log('quickReply: ', message);
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, [msg]),
+    }));
 
     Dialogflow_V2.requestQuery(
       message,
@@ -85,6 +234,19 @@ class App extends Component {
     );
   }
 
+  renderBubble = props => {
+    return (
+      <Bubble
+        {...props}
+        textStyle={{right: {color: 'white'}}}
+        wrapperStyle={{
+          left: {bakgroundColor: 'yellow'},
+          right: {bakgroundColor: 'pink'},
+        }}
+      />
+    );
+  };
+
   render() {
     return (
       <View style={{flex: 1, backgroundColor: '#FFFFFF'}}>
@@ -92,6 +254,7 @@ class App extends Component {
           messages={this.state.messages}
           onSend={message => this.onSend(message)}
           onQuickReply={quickReply => this.onQuickReply(quickReply)}
+          renderBubble={this.renderBubble}
           user={{_id: 1}}
         />
       </View>
